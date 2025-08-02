cb28165f2f542d898db3db6f74b1baf6491088fc
Machelli <valpraiz81@gmail.com>
2025-08-02 14:22:58 -0700

    function added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function()  {
 
     const form = document.getElementById('registration-form');
     
